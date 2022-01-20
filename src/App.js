bce3c6d2fd4c90126c9e0a898067a2b54703b095
John Greer <johnmorrisgreer@gmail.com>
2022-01-19 20:56:21 -0500

    added axios and now we're logging API results to the console

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import './styles/App.css'
+import axios from 'axios'
+import { BASE_URL } from './globals'
 
 const App = () => {
+  const [parks, setParks] = useState([])
+  const API_KEY = process.env.REACT_APP_NPS_KEY
+
+  useEffect( ()=>{
+    async function getParks() {
+      const response = await axios.get(`${BASE_URL}&api_key=${API_KEY}`)
+      setParks(response.data.results)
+      console.log(parks)
+    }
+    getParks()
+  } , [] )
 
   return (
     <div>
@@ -12,4 +25,4 @@ const App = () => {
   );
 }
 
-export default App;
+export default App
