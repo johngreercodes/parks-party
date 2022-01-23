c20b64e3941a0e8a3bfc5bf13518f5c9d9df979c
John Greer <johnmorrisgreer@gmail.com>
2022-01-23 14:07:54 -0500

    piping data to page via StateItem.jsx now

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import axios from 'axios'
 import { BASE_URL } from './globals'
 import ParkItem from './components/ParkItem'
 import StateItem from './components/StateItem'
+import states from './data/states.json'
 
 const App = () => {
   const [parks, setParks] = useState([])
-  const [states, setStates] = useState([])
   const API_KEY = process.env.REACT_APP_NPS_KEY
 
   useEffect( ()=>{
@@ -21,7 +21,11 @@ const App = () => {
   return (
     <div>
       <h2>welcome to the parks party</h2>
-      <StateItem states={states}/>
+      <StateItem 
+        key={states.id}
+        name={states.name}
+        code={states.code}
+      />
       <ParkItem parks={parks}/>
     </div>
   );
--- src/components/StateItem.jsx
+++ src/components/StateItem.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
-import states from '../data/states'
 
-const StateItem = (props) => {
-
-    return (
-        <div>
-            {
-                props.states.map((state)=>(
-                    <div key={state.id}>
-                        <p>{state.name}</p>
-                        <p>{state.code}</p>
-                    </div>
-                ))
-            }
-        </div>
-    )
-}
+const StateItem = (props) => (
+    <div key={props.key}>
+        <button type="button">{props.name}</button>
+    </div>
+)
 
 export default StateItem
