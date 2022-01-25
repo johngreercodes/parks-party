fc67b448c2aef42e52cd6e14e41da66245998811
John Greer <johnmorrisgreer@gmail.com>
2022-01-24 19:54:20 -0500

    snazzing up the park items with css

--- src/components/ParkItem.jsx
+++ src/components/ParkItem.jsx
@@ -5,10 +5,10 @@ const ParkItem = (props) => {
         <div> 
             {
                props.parks.map((park)=>(
-                   <div key={park.id}>
-                       <h1>{park.fullName}</h1>
-                       <div><a href={park.directionsUrl} target="_blank"><button type="button" id="button">Take me there!</button></a></div>
+                   <div key={park.id} id='parkItem'>
                        <img width="50%" src={park.images[0].url} alt={park.fullName}/>
+                       <h1>{park.fullName}</h1>
+                       <a href={park.directionsUrl} target="_blank"><button type="button" id="button">Take me there!</button></a>
                    </div>
                ))
             }
